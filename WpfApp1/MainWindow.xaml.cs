b2eac21d26307acb8be7f54261e3f4213a6c587a
adresz <tigerad97@gmail.com>
2025-02-25 17:05:47 +0100

    Kezdőlap átdolgozva, külnöböző képernyökön nagyjáboli egységes megjelenités megcsinálva, lokális adatb.hez kapcsolat lérehozva.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -19,74 +19,15 @@ namespace WpfApp1;
 /// </summary>
 public partial class MainWindow : Window
 {
+    //Csatlakozás a helyi SQL adatbázishoz
     private string connectionString = "server=localhost;database=userdatabase;uid=root;pwd=;";
     private string username, password;
-
 
     public MainWindow()
     {
         InitializeComponent();
     }
 
-    private void Login_click(object sender, RoutedEventArgs e)
-    {
-        User();
-    }
-
-    private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-    {
-
-    }
-
-    private void User()
-    {
-        username = UsernameTbox.Text;
-        password = PasswordTbox.Password;
-
-        if (AuthenticateUser(username, password))
-        {
-            MessageBox.Show("Sikeres bejelentkezés!", "Belépés", MessageBoxButton.OK, MessageBoxImage.Information);
-        }
-        else
-        {
-            MessageBox.Show("Hibás felhasználónév vagy jelszó!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-    }
-
-    private void Register_click(object sender, RoutedEventArgs e)
-    {
-
-    }
-
-    private bool AuthenticateUser(string username, string password)
-    {
-        try
-        {
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
-            {
-                conn.Open();
-                string query = "SELECT password FROM users WHERE username = @username LIMIT 1";
-
-                using (MySqlCommand cmd = new MySqlCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@username", username);
-
-                    object result = cmd.ExecuteScalar();
-
-                    if (result != null)
-                    {
-                        string hashedPassword = result.ToString();
-                        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
-                    }
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            MessageBox.Show("Hiba történt az adatbázis kapcsolat során: " + ex.Message, "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-        return false;
-    }
 
 
-    }
+}
