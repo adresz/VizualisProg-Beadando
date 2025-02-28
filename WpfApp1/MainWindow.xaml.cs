d1bf9f2c53d0f91adef6c3c010e7ccfe756c96bb
adresz <tigerad97@gmail.com>
2025-02-28 20:02:32 +0100

    Adatbázis hozzáférés átalakitva EF-re, hibakezelés hozzáadva, regisztráció gomb implementáció (menüje még kidolgozásra vár)

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -9,11 +9,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BCrypt.Net;
-using MySql.Data.MySqlClient;
 using WpfApp1;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
-
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using System;
+using System.ComponentModel.DataAnnotations;
+using WpfApp1.RegisterView;
 
 namespace LoginOptions;
 
@@ -29,6 +31,67 @@ public partial class MainWindow : Window
 
     private void Login_Click(object sender, RoutedEventArgs e)
     {
+        try
+        {
+            using (var db = new AppDBContext())
+            {
+                var user = db.users.FirstOrDefault(u => u.Username == Username.Text);
+
+                if (user != null && BCrypt.Net.BCrypt.Verify(Password.Password, user.Password) && (user.AccessID == 3 || user.AccessID == 2))
+                {
+
+                    MessageBox.Show("Sikeres bejelentkezés");
+
+                }
+                else
+                {
+                    MessageBox.Show("Hibás felhasználónév vagy jelszó");
+                }
+
+            }
+
+        }
+        catch (Exception err)
+        {
+            MessageBox.Show("Hiba történt,kérjük lépjen kapcsolatba az ügyfélszolgálattal: +\n" + err);
+        }
+    }
+
+    private void Register_Click(object sender, RoutedEventArgs e)
+    {
+        Register RegisterWindow = new Register();
+        RegisterWindow.Show();
+        this.Close();
+    }
+}
+
+
+public class User
+{
+    [Key] // Enélkül össze szarja magát az adatbázis, ne nyúlj hozzá 
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int AccessID { get; set; }
+    
+
+
+
+    
+}
+
+public class AppDBContext : DbContext
+{
+    public DbSet<User> users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
         
+        var connectionString = "server=localhost;database=userdatabase;user=root;password=;";
+
+
+        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+        
+
     }
 }
