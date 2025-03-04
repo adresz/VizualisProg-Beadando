19f886b8feb951f23a4919f3abbf7a3c77bc74f2
SzaboArpi <133884511+SzaboArpi@users.noreply.github.com>
2025-03-04 10:10:56 +0100

    Regisztráció ablak megcsinálva

--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59651418a3afc066fd25cf688657599a2a22b094")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a812e045a4bc1e0bbe11d89716f471c98253f1db")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
