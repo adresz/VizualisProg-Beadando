7e670109059d26e3970924b143c766ba3a7efe24
adresz <tigerad97@gmail.com>
2025-03-05 23:55:27 +0100

    Javitás
    Néhány formalitás javitása, egy két hiba helyre rakva, jelszavak össze hasonlitása megoldva
    
    Co-Authored-By: SzaboArpi <133884511+SzaboArpi@users.noreply.github.com>

--- WpfApp1/RegisterView/Register.xaml.cs
+++ WpfApp1/RegisterView/Register.xaml.cs
@@ -44,7 +44,7 @@ namespace WpfApp1.RegisterView
         {
             if (ValidateFields())
             {
-                MessageBox.Show("Sikeres bejelentkezés");
+                MessageBox.Show("Sikeres regisztrálás");
             }
         }
 
@@ -52,31 +52,62 @@ namespace WpfApp1.RegisterView
         {
             List<TextBox> textBoxes = new List<TextBox> { LastName, FirstName, Username, Email, Phone, ID};
             List<PasswordBox> passwordBoxes = new List<PasswordBox> { Password, Passwordconf};
-            
+
             //ehhez már túl fáradt vagyok
             //hogy kell jelszavakat összehasonlítani?
-            if(Password.Name == Passwordconf.Name)
+            //Kb 20 percig szenvedtem, hogy rábirjam az igazra,
+            //mostmár elvileg müködik ez a csodás bogár :D
+
+            if (string.IsNullOrEmpty(Password.Password) || string.IsNullOrEmpty(Passwordconf.Password) || Password.Password != Passwordconf.Password)
             {
                 passwordBoxes[0].BorderBrush = Brushes.Red;
+                passwordBoxes[0].BorderThickness = new Thickness(2);
                 passwordBoxes[1].BorderBrush = Brushes.Red;
+                passwordBoxes[1].BorderThickness = new Thickness(2);
                 return false;
             }
+            else
+            {
+                passwordBoxes[0].BorderBrush = Brushes.Gray;
+                passwordBoxes[0].BorderThickness = new Thickness(1);
+                passwordBoxes[1].BorderBrush = Brushes.Gray;
+                passwordBoxes[1].BorderThickness = new Thickness(1);
+            }
 
-            string tmp = "";
+            /*
+            Itt meg nem akarja mindegyik mezőre kiirni
+             csak a legfelsőre ami nincs kitöltve, igy kéne lennie?
+            továbbá
+            az eredeti kódod hibára futott ha hiányzott a második mezőből bármi, szóval kicsit átdolgoztam
+            */
             foreach (var textBox in textBoxes)
             {
-                tmp = "";
+                string errorTextBlockName = textBox.Name + "_err";
+                TextBlock errorTextBlock = (TextBlock)FindName(errorTextBlockName);
+
                 if (string.IsNullOrEmpty(textBox.Text))
                 {
                     textBox.BorderBrush = Brushes.Red;
-                    tmp = (string)textBox.GetValue(FrameworkElement.NameProperty) + "_err";
-                    TextBlock targetTextBox = (TextBlock)FindName(tmp);
-                    targetTextBox.Visibility = Visibility.Visible;
 
+                    if (errorTextBlock != null)
+                    {
+                        errorTextBlock.Visibility = Visibility.Visible;
+                    }
+
+                    
                     return false;
                 }
+                else
+                {
+                    textBox.ClearValue(BorderBrushProperty);
+                    if (errorTextBlock != null)
+                    {
+                        errorTextBlock.Visibility = Visibility.Collapsed;
+                    }
+                }
             }
-            
+            //Kell még dátum ellenőrzés majd, meg jelszó komplexitás ellenőrzés
+
             //ellenőrzés kell, hogy adatbázisban létezik-e
             return true;
         }
--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e94e27b798b4f1021b13e6f84b60a4bf1dba5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9e6880388155d332b27e828747f1293a37c2bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
