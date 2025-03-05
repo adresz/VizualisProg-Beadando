48c10c98c828c8b62faf4c54b05ce6392cc6343f
SzaboArpi <133884511+SzaboArpi@users.noreply.github.com>
2025-03-05 22:22:41 +0100

    regisztráló ablak
    Ablakméret már nem változik.
    
    A passwordboxokkal nem tudom mi történik.
    
    Co-Authored-By: adresz <32986246+adresz@users.noreply.github.com>

--- WpfApp1/RegisterView/Register.xaml.cs
+++ WpfApp1/RegisterView/Register.xaml.cs
@@ -25,20 +25,70 @@ namespace WpfApp1.RegisterView
         public Register()
         {
             InitializeComponent();
+<<<<<<< Updated upstream
         }
 
+=======
+            Birthday.DisplayDateEnd = DateTime.Today;
+            Birthday.DisplayDateStart = DateTime.Parse("1900.01.01");
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+>>>>>>> Stashed changes
         private void GoBack_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.MainWindow.Show();
             this.Close();
-
         }
 
         private void Register_Click(object sender, RoutedEventArgs e)
         {
+<<<<<<< Updated upstream
 
         }
 
 
+=======
+            if (ValidateFields())
+            {
+                MessageBox.Show("Sikeres bejelentkezés");
+            }
+        }
+
+        private bool ValidateFields()
+        {
+            List<TextBox> textBoxes = new List<TextBox> { LastName, FirstName, Username, Email, Phone, ID};
+            List<PasswordBox> passwordBoxes = new List<PasswordBox> { Password, Passwordconf};
+            
+            if(Password.Name == Passwordconf.Name)
+            {
+                passwordBoxes[0].BorderBrush = Brushes.Red;
+                passwordBoxes[1].BorderBrush = Brushes.Red;
+                return false;
+            }
+
+            string tmp = "";
+            foreach (var textBox in textBoxes)
+            {
+                tmp = "";
+                if (string.IsNullOrEmpty(textBox.Text))
+                {
+                    textBox.BorderBrush = Brushes.Red;
+                    tmp = (string)textBox.GetValue(FrameworkElement.NameProperty) + "_err";
+                    TextBlock targetTextBox = (TextBlock)FindName(tmp);
+                    targetTextBox.Visibility = Visibility.Visible;
+
+                    return false;
+                }
+            }
+            
+            //ellenőrzés kell, hogy adatbázisban létezik-e
+            return true;
+        }
+>>>>>>> Stashed changes
     }
 }
--- WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19f886b8feb951f23a4919f3abbf7a3c77bc74f2")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33ec76c5b83ab3826c398f4f521d9f2aa71b332")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
