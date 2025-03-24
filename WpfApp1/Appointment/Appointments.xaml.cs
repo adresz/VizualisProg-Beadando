a7bc1d50304803aa7d639910b87113cfa8cd41d5
adresz <tigerad97@gmail.com>
2025-03-24 16:14:47 +0100

    Update
    Mostmár megvan designolva az alap időpont foglaló oldal, már kijelzi a foglalható időpontokat, a közvetlen következő és korábbi órákat nem, hogy ne lehessen oda foglalni.
    
    Co-Authored-By: MeZeteny <198044126+MeZeteny@users.noreply.github.com>

--- WpfApp1/Appointment/Appointments.xaml.cs
+++ WpfApp1/Appointment/Appointments.xaml.cs
@@ -1,28 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1.Appointment
 {
-    /// <summary>
-    /// Interaction logic for Appointments.xaml
-    /// </summary>
+    public class MenuItem
+    {
+        public string Time { get; set; }
+        public string Description { get; set; }
+    }
+
     public partial class Appointments : Window
     {
+        public ObservableCollection<MenuItem> MenuItems { get; set; } = new ObservableCollection<MenuItem>();
+
         public Appointments(string doctor)
         {
             InitializeComponent();
             Title = $"Választott orvosa: {doctor}";
+
+            DataContext = this;
+            UpdateMenu();
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMinutes(1) // Percenként frissítés
+            };
+            timer.Tick += (sender, args) => UpdateMenu();
+            timer.Start();
+        }
+
+        private void UpdateMenu()
+        {
+            string isAvailable = "Elérhető";
+            int currentHour = DateTime.Now.Hour;
+            MenuItems.Clear();
+
+            for (int hour = 8; hour <= 18; hour++)
+            {
+                //Ne lehessen foglalni a következő órára, csak 2 vel későbbire
+                if (hour > currentHour+1)
+                {
+                    // Az összes órát hozzáadjuk, nem csak a jövőbeli időpontokat
+                    MenuItems.Add(new MenuItem { Time = $"{hour}:00", Description = $"{isAvailable} időpont" });
+                }
+                
+            }
+        }
+
+        // Foglalás gomb eseménykezelője
+        private void BookAppointment_Click(object sender, RoutedEventArgs e)
+        {
+            // A gomb Tag-jából lekérjük az időpontot
+            var button = sender as Button;
+            var time = button?.Tag.ToString();
+
+            MessageBox.Show($"A foglalás sikeresen megtörtént: {time}");
+        }
+
+        private void LogOut_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        private void GoBack_click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
